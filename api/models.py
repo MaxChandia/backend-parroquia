7196953799d6f09c438ba7c6f3ed33670ab9e6af
MaxChandia <maximiliano.chandiaf@gmail.com>
2025-10-22 09:15:49 -0300

    feat/main: cambios django

--- api/models.py
+++ api/models.py
@@ -27,7 +27,6 @@ class Post(models.Model):
     content = models.TextField()
     createdAt = models.DateTimeField(default=timezone.now)
     imageUrls = models.JSONField(default=list) 
-    
     slug = models.SlugField(max_length=255, unique=True)
     title = models.CharField(max_length=255)
 
--- backendtransfiguracion/settings.py
+++ backendtransfiguracion/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1wk)6hkge#3@8f$9judmda0-vjsu9#+1kahln5iu7-nwbb*6-1'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = []
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
@@ -75,11 +81,18 @@ WSGI_APPLICATION = 'backendtransfiguracion.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
+'default': dj_database_url.config(
+
+# Si no hay DATABASE_URL, usa tu SQLite local para desarrollo
+
+default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
+
+conn_max_age=600 # Opcional: tiempo de vida de la conexión
+
+)
+
+} 
 
 
 # Password validation
